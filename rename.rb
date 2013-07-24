9e7925ede98f47ad354b831d5062d8284f936374
404pnf <404pnf@exmaple.org>
2013-07-24 17:31:11 +0800

    增加说明

--- rename.rb
+++ rename.rb
@@ -1,5 +1,12 @@
 #!/usr/bin/env ruby
 #
+
+
+# 404pnf: original found at: http://www.technetra.com/2008/11/11/codemagic-renamerb-a-ruby-version-of-perls-rename-command/
+# 404pnf: an one-liner: http://www.psteiner.com/2012/05/how-to-rename-files-with-ruby.html
+# ruby -e 'Dir.glob("IP*.xml").each { |f| File.rename(f,f[24..-1]) }'
+
+
 # rename.rb is a Ruby version of the popular Perl command found in many
 # modern Linux distributions.
 #
